1ab8257230078d318f5cf79ffaad10e56ffe2a04
doomibox <kevinhu1997@gmail.com>
2024-09-17 06:21:24 +0000

    add hardTextScanner

--- hardTextScanner.py
+++ hardTextScanner.py
@@ -4,12 +4,17 @@ import json
 import logging
 import time
 import os
+import markdown
+import markdown.extensions.fenced_code
+
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ['OPEN_AI_KEY'])
+
 
 import threading
 
 from flask import Flask, jsonify, request, g, render_template
-from wordfreq import zipf_frequency
-from PyDictionary import PyDictionary
 
 
 
@@ -26,84 +31,37 @@ app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 # dictionary setup
-dictionary = PyDictionary()
+# dictionary = PyDictionary()
 #dictionary.set_words_lang('en')
 
+# llm setup
+system_prompt = {"role": "system", "content": "Your job is to pick the top 10 hardest words from the user-provided text and come up with 3 synonyms for each of the hardes words you choose. Return in format of html language directly."}
 
-# main app
-@app.route('/hardTextScan', methods=['POST'])
-def index():
+
+@app.route('/hardTextScanLlm', methods=['POST'])
+def scan_with_llm():
     data = request.json
     text = data['payload']
-    word_dict = dict()
 
-    word_freq_start = time.time()
-    for word_literal in text.split():
-        if _is_word_eligible(word_literal):
-            for word in _preprocess_word(word_literal):
-                if word not in word_dict:
-                    freq = zipf_frequency(word, 'en')
-                    word_dict[word] = {"freq":freq, "count":1}
-                else:
-                    word_dict[word]["count"] += 1
+    messages = [
+        system_prompt,
+        {"role": "user", "content": text}
+    ]
 
-    sorted_list = sorted(word_dict.items(), key=lambda x:x[1]["freq"])
-    filtered_list = list(filter(lambda x:x[1]['freq'] != 0, sorted_list))
-    word_freq_end = time.time()
-    word_freq_diff = (word_freq_end - word_freq_start) * 1000
+    response = client.chat.completions.create(
+        messages=messages,
+        temperature=0.1,
+        model="gpt-3.5-turbo",
+    )
+    response_message = response.choices[0].message.content.encode('utf-8').decode()
 
-    #log.info(list(map(lambda x:x[0], filtered_list)))
-    log.info("WordFreq Elapsed %.1f ms" % word_freq_diff)
-    
-    
-    translate_start = time.time()
-    response = list()
-    for item in filtered_list:
-        formated_item = _format_item(item)
-        if formated_item['explain'] != None and len(formated_item['explain']) != 0:
-            response.append(formated_item)
-            if len(response) == 10:
-                break
-    translate_end = time.time()
-    translate_diff = (translate_end - translate_start) * 1000
-    log.info("Translate Elapsed %.1f ms" % translate_diff)
-            
-    #log.info(response)
-    return render_template('result.html', items=response)
+    #md_template_string = markdown.markdown(response_message)
+
+    print(response_message)
+    return response_message
 
 
-def _is_word_eligible(word):
-    # todo: exclude names
-    return word.isalpha()  
 
-def _preprocess_word(word):
-    res = []
-    word = word.lower()
-    if _not_empty(word) and word[-1] in ['.', ',', '?', '!', ')']:
-        word = word[:-1] # remove appended punctuation
-    if _not_empty(word) and word[0] in ['(', '@']:
-        word = word[1:]
-    if _not_empty(word) and '-' in word:
-        res = word.split('-')
-    res.append(word.replace('-', '')) # have potentially anti-social => [anti, social, antisocial]
-    return res
-        
-def _not_empty(word):
-    return word != None and len(word) > 0
-
-def _format_item(item):
-    return {
-        "word": item[0], 
-        "score": item[1]['freq'], 
-        "explain": _get_meaning(item[0])
-    }
-
-def _get_meaning(word):
-    try:
-        #return dictionary.meaning('en', word, dictionary='wordnet')
-        return dictionary.meaning(word)
-    except Exception:
-        return {}
     
 
 @app.before_request
